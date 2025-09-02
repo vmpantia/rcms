d726f44f26017755b90256ac2aa2e5245c166502
Vincent Pantia <vincent.m.pantia@hotmail.com>
2025-09-02 12:38:15 +0800

    Minor: Update UI for saving course.

--- RCMS/RCMS.Shared/Models/Courses/SaveCourseDto.cs
+++ RCMS/RCMS.Shared/Models/Courses/SaveCourseDto.cs
@@ -2,7 +2,7 @@ namespace RCMS.Shared.Models.Courses;
 
 public class SaveCourseDto
 {
-    public Guid CategoryId { get; set; }
     public string Name { get; set; }
+    public string CategoryId { get; set; }
     public string? Description { get; set; }
 }
--- RCMS/RCMS.Web/Models/SelectFieldOption.cs
+++ RCMS/RCMS.Web/Models/SelectFieldOption.cs
@@ -1,7 +1,44 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
 namespace RCMS.Web.Models;
 
-public class SelectFieldOption<TValue>
+public class SelectFieldOption
 {
-    public TValue Value { get; set; }
-    public string Display { get; set; }
+    public SelectFieldOption(string value, RenderFragment display)
+    {
+        Value = value;
+        Display = display;
+    }
+
+    public SelectFieldOption(string value)
+    {
+        Value = value;        
+        Display = builder =>
+        {
+            builder.OpenComponent<MudText>(0);
+            builder.AddAttribute(1, "ChildContent", (RenderFragment)(childBuilder =>
+            {
+                childBuilder.AddContent(2, value);
+            }));
+            builder.CloseComponent();
+        };
+    }
+
+    public SelectFieldOption(string value, string display)
+    {
+        Value = value;        
+        Display = builder =>
+        {
+            builder.OpenComponent<MudText>(0);
+            builder.AddAttribute(1, "ChildContent", (RenderFragment)(childBuilder =>
+            {
+                childBuilder.AddContent(2, display);
+            }));
+            builder.CloseComponent();
+        };
+    }
+    
+    public string Value { get; set; }
+    public RenderFragment Display { get; set; }
 }
